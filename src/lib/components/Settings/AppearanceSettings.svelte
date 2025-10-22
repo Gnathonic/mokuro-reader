6e953c704f66955550f2f72bb1e20b704247d118
Oscar Wong <oscarwong67@gmail.com>
2025-10-21 23:17:52 -0700

    add toggle

---
+++ src/lib/components/Settings/AppearanceSettings.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { settings, updateSetting } from '$lib/settings';
+  import { AccordionItem, Toggle } from 'flowbite-svelte';
+  let darkMode = $settings.darkMode;
+</script>
+
+<AccordionItem>
+  <span slot="header">Appearance</span>
+  <div class="flex flex-col gap-5">
+    <div>
+      <Toggle bind:checked={darkMode} on:change={() => {
+        updateSetting('darkMode', darkMode)
+      }}>Dark Mode</Toggle>
+    </div>
+  </div>
+</AccordionItem>
+
+
